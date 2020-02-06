c3039b98133a9591b06db782b5acf2507bcdf710
Adam Scott <ascott1@gmail.com>
2020-02-06 06:35:56 -0500

    add EJS example

--- 22-express/using-express/index.js
+++ 22-express/using-express/index.js
@@ -13,9 +13,24 @@ app.use(express.static('files'));
 // use Pug as a templating engine
 app.set('view engine', 'pug');
 
+// uncomment to see examples with EJS:
+// app.set('view engine', 'ejs');
+
 // respond with the template
 app.get('/template', (req, res) => {
   res.render('template');
+});
+
+// a user object of data to send to the template
+const userData = {
+  name: 'Adam',
+  email: 'adam@jseverywhere.io',
+  avatar: 'https://s.gravatar.com/avatar/33aab819d1ffa11fc4b31a4eebaf0c5a?s=80'
+};
+
+// render the template with user data
+app.get('/user', (req, res) => {
+  res.render('user', { userData });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
