4f84b485a4c0fc027e962d996bcea3e9d8a52da0
Adam Scott <ascott1@gmail.com>
2020-02-17 07:30:25 -0500

    fix bug in advanced read-write example

--- 18-node-basics/18-2-reading-writing-file-data/advanced-open-read-write.js
+++ 18-node-basics/18-2-reading-writing-file-data/advanced-open-read-write.js
@@ -1,26 +1,25 @@
-// TODO: This is a ES6+ translation of the code in E2
-// consider using promises to simplify
+/* eslint-disable no-shadow */
 const fs = require('fs');
 
 fs.open('newfile.txt', 'a+', (err, fd) => {
   if (err) {
     throw err;
   } else {
-    const buf = Buffer.from('The first string');
-    fs.write(fd, buf, 0, buf.length, 0, (errWrite, written) => {
-      if (errWrite) {
-        throw errWrite;
+    const buf = Buffer.from('The first string\n');
+    fs.write(fd, buf, 0, buf.length, 0, (err, written) => {
+      if (err) {
+        throw err;
       } else {
-        const buf2 = Buffer.from('The second string');
-        fs.write(fd, buf2, 0, buf2.length, 0, (errWrite2, written2) => {
-          if (errWrite2) {
-            throw errWrite2;
+        const buf2 = Buffer.from('The second string\n');
+        fs.write(fd, buf2, 0, buf2.length, buf.length, (err, written2) => {
+          if (err) {
+            throw err;
           } else {
             const length = written + written2;
             const buf3 = Buffer.alloc(length);
-            fs.read(fd, buf3, 0, length, 0, errRead => {
-              if (errRead) {
-                throw errRead;
+            fs.read(fd, buf3, 0, length, 0, err => {
+              if (err) {
+                throw err;
               } else {
                 console.log(buf3.toString());
               }
