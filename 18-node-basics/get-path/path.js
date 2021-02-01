2fd9e272323842350062202ecb7d05bb206925db
Adam Scott <ascott1@gmail.com>
2021-02-01 10:15:29 -0500

    env variables and get path examples

---
+++ 18-node-basics/get-path/path.js
@@ -0,0 +1,7 @@
+// logs the directory of the currently executed file
+// ex: /Users/Adam/Projects/js-cookbook/node
+console.log(__dirname);
+
+// logs the directory and filename of the currently executed file
+// ex: /Users/Adam/Projects/js-cookbook/node/example.js
+console.log(__filename);
