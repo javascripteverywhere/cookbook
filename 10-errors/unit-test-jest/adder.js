f17419cf80cef3116357c61bf1cf3712f73f76e8
Matthew M <matthew@prosetech.com>
2021-05-01 11:13:10 -0400

    ch10 examples

---
+++ 10-errors/unit-test-jest/adder.js
@@ -0,0 +1,14 @@
+function addAndRound(value1, value2) {
+  return Math.round(value1 + value2);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function sum3(a, b) {
+  return a + b;
+}
+
+
+export {sum};
